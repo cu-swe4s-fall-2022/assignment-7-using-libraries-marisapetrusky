826acf4fc0e01168085cfc200f84ba2b0a4fbabe
marisapetrusky <marisa.petrusky@colorado.edu>
2022-10-27 04:12:47 +0000

    Fill random matrix, set unit test

--- data_processor.py
+++ data_processor.py
@@ -1,9 +1,18 @@
 import random
 import numpy as np
 import sys
+import pdb
 
+# Return num_rows x num_columns matrix of entries between 0 and 1
 def get_random_matrix(num_rows, num_columns):
-	return None
+    if not isinstance(num_rows, int) or not isinstance(num_columns, int):
+        return None
+    else:
+        arr = np.empty([num_rows, num_columns])
+        for row in range(num_rows):
+            for col in range(num_columns):
+                arr[row][col] = random.random()
+        return arr
 
 def get_file_dimensions(file_name):
 	return (0,0)
--- tests/test_data_processor.py
+++ tests/test_data_processor.py
@@ -1,6 +1,8 @@
 import unittest
 import os
 import sys
+import random
+import pdb
 sys.path.append("..")
 import data_processor as proc #nopep8
 
@@ -9,6 +11,13 @@ class TestDataProcessor(unittest.TestCase):
     def test_get_rand_mat_empty(self):
         r = proc.get_random_matrix(None, None)
         self.assertEqual(r, None)
+
+    def test_get_rand_mat_fill(self):
+        random.seed(1)
+        randno = random.random()
+        random.seed(1)
+        r = proc.get_random_matrix(14, 14)
+        self.assertEqual(r[0][0], randno)
 
 if __name__ == '__main__':
     unittest.main()
